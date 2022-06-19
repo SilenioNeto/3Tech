4de35d3cc7fc8264e50cbc979005627c3cb12de2
Silenio Neto <surfsilenio@gmail.com>
2022-06-19 16:42:46 -0300

    3Tech concluido

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+
+<body>
+    <h2>UPLOAD</h2>
+    <form action="inserir.php" method="POST" enctype="multipart/form-data">
+    <div><input type="text" name="titulo"></div>
+    <div><input type="text" class="form-control" name="descricao" id="descricao"></div>
+    <div><input type="file" class="form-control" name="imagem" id="imagem"></div>
+    <div><input type="submit" name="cadastrar"></div>
+    </form>
+</body>
+
+<?php
+include('lista.php');
+?>
